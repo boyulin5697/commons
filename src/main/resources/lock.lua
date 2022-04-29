17c63d3281a6ac41cc65f070dd7823b1ba580634
boyulin <1404916254@qq.com>
2022-04-29 11:23:07 +0800

    update with new feature.

--- src/main/resources/lock.lua
+++
@@ -1,8 +0,0 @@
-if (redis.call('exists', KEYS[1]) == 1) then
-     local lock = tonumber(redis.call('get', KEYS[1]));
-     if (lock > 0) then
-        redis.call('incrby', KEYS[1], -1);
-        return lock;
-     end;
-      return 0;
-  end;
